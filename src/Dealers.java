118b25a45017e65608859c191f9d8a7043c024d9
RadekDl <radek.dl@atlas.cz>
2024-09-28 16:53:30 +0200

    Initial commit

---
+++ src/Dealers.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+public class Dealer {
+    private String name;
+    private LocalDate birthday;
+    private String city;
+    private int contracts;
+    private  double weight;
+
+}
